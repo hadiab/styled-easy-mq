9e749dd813e6e44985a1906b9c45a0687a7054dd
hadiabway <abed@givingway.com>
2021-07-07 09:46:03 +0300

    adding csstype

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-export const Greeter = (name: string) => `Hello ${name}`
+import { StyleObject } from "./types"
+import * as facepaint from "facepaint"
+
+const cssToObject = (cssStr: string, ...args: any[]) => {
+	const regex = /([\w-]*)\s*:\s*([^;]*)/g;
+	let match: any = null
+	let properties: any = {}
+	let argsCounter = 0
+
+	// eslint-disable-next-line no-cond-assign
+	while(match = regex.exec(cssStr)) {
+		let value = match[2].trim()
+
+		if(value === `{${argsCounter}}`) {
+			value = args[argsCounter]
+			argsCounter++
+		}
+
+		properties[match[1]] = value
+	}
+
+	return properties
+}
+
+export const createMediaQuery = (breakpoints: number[]) => {
+	const facepaintMQ = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`))
+
+	const mq = (style: TemplateStringsArray | StyleObject, ...args: any[]) => {
+		if(Array.isArray(style)) {
+			const cssStr = style.map((item, i) => item + (args[i] ? `{${i}}` : ""))
+			const cssObj = cssToObject(cssStr.join(""), ...args)
+			return facepaintMQ(cssObj)
+		}
+
+		return facepaintMQ(style)
+	}
+
+	return mq
+}
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import * as CSS from 'csstype';
+
+interface CSSProperties extends CSS.Properties<string | number> {}
+
+export type StyleObject = {
+	[K in keyof CSSProperties]?: CSSProperties[K] | Array<CSSProperties[K]>
+}
+
